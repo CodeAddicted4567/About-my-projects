2b82c18782d579ecfc632392ab56abb189db27d7
CodeAddicted4567 <soumoparui11@gmail.com>
2024-08-25 19:02:15 +0530

    Something----

---
+++ add2input.js
@@ -0,0 +1,16 @@
+//joint 2 strings
+
+function concate(){
+    let str1=prompt("Enter a Name:");
+    let str2=prompt("Enter a Your hobby:");
+    alert(str1+" is a "+str2);
+}
+
+//add 2 numbers:
+
+function add2(){
+    let num1=Number(prompt("Enter a 1st num:"));
+    let num2=Number(prompt("Enter a 2nd num:"));
+    let total=(num1+num2);
+    alert(total);
+    }
